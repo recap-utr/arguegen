92bbc392ae504d0a5d975cd905ee14557feeb1b5
Mirko Lenz <info@mirko-lenz.de>
2020-01-12 22:13:03 +0100

    Add preliminary adaptation support

--- recap_argument_graph_adaptation/__main__.py
+++ recap_argument_graph_adaptation/__main__.py
@@ -1,39 +1,113 @@
+import typing as t
+from dataclasses import dataclass
 from pathlib import Path
 
-import nltk
+import neo4j
 import recap_argument_graph as ag
-import typing as t
+import spacy
+import stackprinter
 
-from .database import Database
+from . import conceptnet
 from .config import Config
+from .database import Database
 
 config = Config.instance()
+db = Database("en")
+nlp = spacy.load("en")
+
+import stackprinter
+
+stackprinter.set_excepthook(style="darkbg")
 
 
 def concepts_in_graph(graph: ag.Graph) -> t.Set[str]:
     concepts = set()
 
     for node in graph.inodes:
-        pos_tags = nltk.pos_tag(nltk.word_tokenize(node.plain_text))
+        for token in node.text:
+            if token.tag_.startswith("N") and not token.is_stop:
+                concepts.add(token.text)
 
-        for word, tag in pos_tags:
-            if tag.startswith("N"):
-                concepts.add(word)
+        # for chunk in node.text.noun_chunks:
+        #     concepts.add(chunk.root.text)
 
     return concepts
 
 
-graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"))
-concepts = concepts_in_graph(graph)
+@dataclass(frozen=True)
+class ConceptAdaptation:
+    adapt_from: str
+    adapt_to: str
+    name: str
 
-db = Database("en")
-concept_paths = {}
 
-for concept1 in concepts:
-    for concept2 in concepts:
-        if concept1 != concept2:
-            concept_paths[(concept1, concept2)] = db.get_shortest_path(
-                concept1, concept2
+def concept_paths(
+    concepts: t.Iterable[str], adaptation_rules: t.Iterable[t.Tuple[str, str]]
+) -> t.Dict[ConceptAdaptation, t.Optional[neo4j.Path]]:
+    paths = {}
+
+    for adapt_from, adapt_to in adaptation_rules:
+        for concept in concepts:
+            if adapt_from != concept:
+                concept_adaptation = ConceptAdaptation(adapt_from, adapt_to, concept)
+                paths[concept_adaptation] = db.shortest_path(adapt_from, concept)
+
+    return paths
+
+
+case_graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"), nlp)
+adapted_graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"))
+query_graph = ag.Graph("Query")
+query_graph.add_node(
+    ag.Node(
+        query_graph.keygen(),
+        nlp(
+            "Any punishment is a legal means that as such is not practicable in Germany."
+        ),
+        ag.NodeCategory.I,
+    )
+)
+
+case_concepts = concepts_in_graph(case_graph)
+query_concepts = concepts_in_graph(query_graph)
+
+# TODO: How to deal with empty replacements: death -> None
+# TODO: How to determine multi-token concepts: death_penalty
+# TODO: allShortestPaths could be used to filter for paths that contain names of existing concepts in graph
+# TODO: Selection of the final adapted relationship is random: next()
+
+adaptation_rules = [("penalty", "punishment"), ("death", "any")]
+original_paths = concept_paths(case_concepts, adaptation_rules)
+adapted_paths = {}
+
+for concept_adaptation, original_path in original_paths.items():
+    adapted_path = db.single_path(concept_adaptation.adapt_to)
+
+    for original_relationship in original_path:
+        adapted_relationships = db.expand_node(
+            adapted_path.end_node, [original_relationship.type]
+        )
+
+        adapted_relationship = next(iter(adapted_relationships), None)
+
+        if adapted_path and adapted_relationship:
+            adapted_path = db.extend_path(adapted_path, adapted_relationship)
+
+    adapted_paths[concept_adaptation] = adapted_path
+
+
+adaptation_rule = adaptation_rules[0]
+for concept_adaptation, adapted_path in adapted_paths.items():
+    if (
+        concept_adaptation.adapt_from == adaptation_rule[0]
+        and concept_adaptation.adapt_to == adaptation_rule[1]
+    ):
+        for node in adapted_graph.inodes:
+            node.text = node.text.replace(
+                concept_adaptation.name,
+                conceptnet.adapt_name(
+                    adapted_path.end_node["name"], concept_adaptation.name
+                ),
             )
 
-print(concept_paths)
+adapted_graph.render(Path("data/out/"))
--- recap_argument_graph_adaptation/database.py
+++ recap_argument_graph_adaptation/database.py
@@ -1,6 +1,7 @@
 import neo4j
 from .config import config
 from . import conceptnet
+from gibberish import Gibberish
 import typing as t
 
 
@@ -14,20 +15,14 @@ class Database:
         )  # auth=("username", "password")
         self.lang = lang
 
-    def get_concept(self, name: str) -> t.Optional[neo4j.Node]:
-        with self._driver.session() as session:
-            return session.read_transaction(self.match_concept_node, name, self.lang)
+    # NODE
 
-    def get_shortest_path(self, start: str, end: str) -> t.Optional[neo4j.Path]:
+    def node(self, name: str) -> t.Optional[neo4j.Node]:
         with self._driver.session() as session:
-            return session.read_transaction(
-                self.match_shortest_path, start, end, self.lang
-            )
+            return session.read_transaction(self._node, name, self.lang)
 
     @staticmethod
-    def match_concept_node(
-        tx: neo4j.Session, name: str, lang: str
-    ) -> t.Optional[neo4j.Node]:
+    def _node(tx: neo4j.Session, name: str, lang: str) -> t.Optional[neo4j.Node]:
         result = tx.run(
             "MATCH (n:Concept {name: $name, language: $lang}) RETURN n",
             name=conceptnet.concept_name(name, lang),
@@ -36,8 +31,14 @@ class Database:
 
         return result.value() if result else None
 
+    # SHORTEST PATH
+
+    def shortest_path(self, start: str, end: str) -> t.Optional[neo4j.Path]:
+        with self._driver.session() as session:
+            return session.read_transaction(self._shortest_path, start, end, self.lang)
+
     @staticmethod
-    def match_shortest_path(
+    def _shortest_path(
         tx: neo4j.Session, start: str, end: str, lang: str
     ) -> t.Optional[neo4j.Path]:
         result = tx.run(
@@ -48,5 +49,72 @@ class Database:
             # max_relations=max_relations,
             lang=lang,
         ).single()
+
+        return result.value() if result else None
+
+    # SINGLE PATH
+
+    def single_path(self, name: str) -> t.Optional[neo4j.Path]:
+        with self._driver.session() as session:
+            return session.read_transaction(self._single_path, name, self.lang)
+
+    @staticmethod
+    def _single_path(tx: neo4j.Session, name: str, lang: str):
+        result = tx.run(
+            "MATCH p=(n:Concept {name: $name, language: $lang}) RETURN p",
+            name=conceptnet.concept_name(name, lang),
+            lang=lang,
+        ).single()
+
+        return result.value() if result else None
+
+    # EXTEND PATH
+
+    def extend_path(
+        self, path: neo4j.Path, relationship: neo4j.Relationship
+    ) -> neo4j.Path:
+        with self._driver.session() as session:
+            return session.read_transaction(self._extend_path, path, relationship)
+
+    @staticmethod
+    def _extend_path(
+        tx: neo4j.Session, path: neo4j.Path, relationship: neo4j.Relationship
+    ) -> neo4j.Path:
+        wordgen = Gibberish()
+        relationships = list(path.relationships) + [relationship]
+        words = wordgen.generate_words(len(relationships))
+
+        q = "MATCH p = ( (:Concept)-["
+        q += "]-(:Concept)-[".join(words)
+        q += "]-(:Concept)) WHERE "
+
+        where_expr = [f"id({word})={rel.id}" for word, rel in zip(words, relationships)]
+        q += " AND ".join(where_expr)
+
+        q += " RETURN p"
+
+        result = tx.run(q).single()
+
+        return result.value()
+
+    # EXPAND NODE
+
+    def expand_node(
+        self, node: neo4j.Node, rel_types: t.Collection[str]
+    ) -> t.Optional[t.List[neo4j.Relationship]]:
+        with self._driver.session() as session:
+            return session.read_transaction(
+                self._expand_node, node, rel_types, self.lang
+            )
+
+    @staticmethod
+    def _expand_node(
+        tx: neo4j.Session, node: neo4j.Node, rel_types: t.Collection[str], lang: str
+    ) -> t.Optional[t.List[neo4j.Relationship]]:
+        rel_query = "|:".join(rel_types)
+        result = tx.run(
+            f"MATCH (n:Concept {{language: $lang}})-[r:{rel_query}]-(m:Concept {{language: $lang}}) WHERE id(n)={node.id} RETURN r",
+            lang=lang,
+        )
 
         return result.value() if result else None
