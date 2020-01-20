234f9af3a5b0ecdf5d3760ff63e4edff64ef9fe2
Mirko Lenz <info@mirko-lenz.de>
2020-01-20 23:43:21 +0100

    Refactor the adaptation process

---
+++ recap_argument_graph_adaptation/controller/concept.py
@@ -0,0 +1,65 @@
+import neo4j
+import recap_argument_graph as ag
+import typing as t
+
+from ..model import conceptnet
+from ..model.database import Database
+
+
+def from_graph(graph: ag.Graph) -> t.Set[str]:
+    concepts = set()
+
+    for node in graph.inodes:
+        for token in node.text:
+            if token.tag_.startswith("N") and not token.is_stop:
+                concepts.add(token.text)
+
+        # for chunk in node.text.noun_chunks:
+        #     concepts.add(chunk.root.text)
+
+    return concepts
+
+
+def reference_paths(
+    db: Database, concepts: t.Iterable[str], rule: t.Tuple[str, str]
+) -> t.Dict[str, t.Optional[neo4j.Path]]:
+    return {
+        concept: db.shortest_path(rule[0], rule[1])
+        for concept in concepts
+        if rule[0] != concept
+    }
+
+
+def adapt_paths(
+    db: Database,
+    reference_paths: t.Dict[str, t.Optional[neo4j.Path]],
+    rule: t.Tuple[str, str],
+) -> t.Dict[str, t.Optional[neo4j.Path]]:
+    adapted_paths = {}
+
+    for concept, reference_path in reference_paths.items():
+        # We have to convert the target to a path object here.
+        adapted_path = db.single_path(rule[1])
+
+        for reference_rel in reference_path:
+            adapted_rel = db.expand_node(adapted_path.end_node, [reference_rel.type])
+
+            # TODO: REALLY BAD!!
+            adapted_relationship = next(iter(adapted_rel), None)
+
+            if adapted_path and adapted_relationship:
+                adapted_path = db.extend_path(adapted_path, adapted_relationship)
+
+        adapted_paths[concept] = adapted_path
+
+    return adapted_paths
+
+
+def adapt_graph(
+    graph: ag.Graph, adapted_paths: t.Dict[str, t.Optional[neo4j.Path]]
+) -> None:
+    for concept, path in adapted_paths.items():
+        for node in graph.inodes:
+            node.text = node.text.replace(
+                concept, conceptnet.adapt_name(path.end_node["name"], concept),
+            )
