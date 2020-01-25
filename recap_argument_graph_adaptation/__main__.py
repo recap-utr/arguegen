3a36a727166d06d3cbdf59bafce4c91813c769e5
Mirko Lenz <info@mirko-lenz.de>
2020-01-25 19:52:00 +0100

    Remove query as it is not necessary

--- recap_argument_graph_adaptation/__main__.py
+++ recap_argument_graph_adaptation/__main__.py
@@ -12,32 +12,15 @@ config = Config.instance()
 db = Database("en")
 nlp = spacy.load("en_core_web_lg")
 
-# stackprinter.set_excepthook(style="darkbg2")
-
-
-# TODO: allShortestPaths could be used to filter for paths that contain names of existing concepts in graph
-# TODO: Selection of the final adapted relationship is random: next()
-
 case_graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"), nlp)
 adapted_graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"))
-query_graph = ag.Graph("Query")
-query_graph.add_node(
-    ag.Node(
-        query_graph.keygen(),
-        nlp(
-            "Any punishment is a legal means that as such is not practicable in Germany."
-        ),
-        ag.NodeCategory.I,
-    )
-)
 
-case_concepts = concept.from_graph(db, case_graph)
-query_concepts = concept.from_graph(db, query_graph)
+concepts = concept.from_graph(db, case_graph)
 
 adaptation_rules = [("penalty", "punishment")]
 
 for rule in adaptation_rules:
-    reference_paths = concept.reference_paths(db, case_concepts, rule)
+    reference_paths = concept.reference_paths(db, concepts, rule)
     adapted_paths = concept.adapt_paths(db, reference_paths, rule)
     concept.adapt_graph(adapted_graph, adapted_paths)
 
