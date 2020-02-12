a6e44d1c80316e5aba597a08e4cb701a8fd5392b
Mirko Lenz <info@mirko-lenz.de>
2020-02-12 15:48:08 +0100

    Adapt main method

--- recap_argument_graph_adaptation/__main__.py
+++ recap_argument_graph_adaptation/__main__.py
@@ -1,39 +1,39 @@
 import json
+import logging
 from pathlib import Path
 
 import recap_argument_graph as ag
 import spacy
-import logging
 
-from .controller import concept, adapt
-from .model.config import config
+from .controller import adapt, concept, load
 from .model import adaptation
+from .model.config import config
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__package__)
 
 nlp = spacy.load(config["spacy"]["model"])
 
-case_graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"), nlp)
-adapted_graph = ag.Graph.open(Path("data/case-base/nodeset6366.json"))
+cases = load.cases()
 out_path = Path(
-    "data", "out", config["adaptation"]["method"], config["adaptation"]["selector"],
+    config["path"]["output"],
+    config["adaptation"]["method"],
+    config["adaptation"]["selector"],
 )
 
-concepts = concept.from_graph(case_graph)
+for case in cases:
+    concepts = concept.from_graph(case.graph)
+    adaptation_results = {}
+    stats_path = out_path / f"{case.graph.name}.json"
 
-adaptation_rules = {"death penalty": "punishment"}
-adaptation_results = {}
+    for rule in case.rules:
+        reference_paths = concept.paths(concepts, rule)
+        adapted_concepts = adapt.paths(reference_paths, rule)
 
-for rule in adaptation_rules.items():
-    reference_paths = concept.paths(concepts, rule)
-    adapted_concepts = adapt.paths(reference_paths, rule)
+        adapt.argument_graph(case.graph, rule, adapted_concepts)
+        adaptation_results[f"{rule[0]}->{rule[1]}"] = adapted_concepts
 
-    adapt.argument_graph(adapted_graph, rule, adapted_concepts)
-    adaptation_results[f"{rule[0]}->{rule[1]}"] = adapted_concepts
+    case.graph.render(out_path)
 
-adapted_graph.render(out_path)
-stats_path = out_path / f"{adapted_graph.name}.json"
-
-with stats_path.open("w") as f:
-    json.dump(adaptation_results, f, ensure_ascii=False, indent=4)
+    with stats_path.open("w") as f:
+        json.dump(adaptation_results, f, ensure_ascii=False, indent=4)
