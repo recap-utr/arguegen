d32ba085ee99562de4d7baa1d646bbe4e6a11244
Mirko Lenz <info@mirko-lenz.de>
2020-05-16 23:24:07 +0200

    [BROKEN] Use sentence transformers

--- recap_argument_graph_adaptation/controller/load.py
+++ recap_argument_graph_adaptation/controller/load.py
@@ -3,6 +3,7 @@ import csv
 import spacy
 import typing as t
 from pathlib import Path
+from sentence_transformers import SentenceTransformer
 
 import recap_argument_graph as ag
 from recap_argument_graph_adaptation.model import adaptation
@@ -37,11 +38,38 @@ def _parse_rules(path: Path) -> t.List[adaptation.Rule]:
     return rules
 
 
-spacy_models = {"en": "en_core_web_lg", "de": "de_core_news_md"}
+spacy_cache = {"en": None, "de": None}
+spacy_models = {"en": "en_core_web_sm", "de": "de_core_news_sm"}
+transformer_models = {
+    "en": "distiluse-base-multilingual-cased",
+    "de": "distiluse-base-multilingual-cased",
+}
 
 
 def spacy_nlp():
     lang = config["nlp"]["lang"]
-    model = spacy_models[lang]
 
-    return spacy.load(model)
+    if not spacy_cache[lang]:
+        model = spacy.load(spacy_models[lang])
+        model.add_pipe(TransformerModel(lang), first=True)
+
+        spacy_cache[lang] = model
+
+    return spacy_cache[lang]
+
+
+# https://spacy.io/usage/processing-pipelines#custom-components-user-hooks
+# https://github.com/explosion/spaCy/issues/3823
+class TransformerModel(object):
+    def __init__(self, lang):
+        self._model = SentenceTransformer(transformer_models[lang])
+
+    def __call__(self, doc):
+        doc.user_hooks["vector"] = self.vector
+        doc.user_span_hooks["vector"] = self.vector
+        doc.user_token_hooks["vector"] = self.vector
+
+        return doc
+
+    def vector(self, obj):
+        return self._model.encode([str(obj)])[0]
