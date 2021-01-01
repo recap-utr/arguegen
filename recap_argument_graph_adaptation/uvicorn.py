a0452caa9b7da9b46fac8b56291fa628e51f0756
Mirko Lenz <info@mirko-lenz.de>
2021-01-01 17:10:20 +0100

    Change uvicorn server startup

--- recap_argument_graph_adaptation/uvicorn.py
+++ recap_argument_graph_adaptation/uvicorn.py
@@ -22,50 +22,30 @@ default_args = {
 }
 
 
-def _spacy():
-    server = multiprocessing.Process(
-        target=uvicorn.run,
-        args=("recap_argument_graph_adaptation.spacy_server:app",),
-        kwargs={
+@app.command(help="Start a spacy server.")
+def spacy():
+    uvicorn.run(
+        "recap_argument_graph_adaptation.spacy_server:app",
+        **{
             "host": config["resources"]["spacy"]["host"],
             "port": config["resources"]["spacy"]["port"],
             "workers": config["resources"]["spacy"]["workers"],
-            **default_args,
         },
-        daemon=False,
+        **default_args
     )
-    server.start()
-
-
-@app.command(help="Start a spacy server.")
-def spacy():
-    _spacy()
-
-
-def _wordnet():
-    server = multiprocessing.Process(
-        target=uvicorn.run,
-        args=("recap_argument_graph_adaptation.wordnet_server:app",),
-        kwargs={
-            "host": config["resources"]["wordnet"]["host"],
-            "port": config["resources"]["wordnet"]["port"],
-            "workers": config["resources"]["wordnet"]["workers"],
-            **default_args,
-        },
-        daemon=False,
-    )
-    server.start()
 
 
 @app.command(help="Start a WordNet server.")
 def wordnet():
-    _wordnet()
-
-
-@app.command(help="Start both spacy and WordNet servers.")
-def all():
-    _wordnet()
-    _spacy()
+    uvicorn.run(
+        "recap_argument_graph_adaptation.wordnet_server:app",
+        **{
+            "host": config["resources"]["wordnet"]["host"],
+            "port": config["resources"]["wordnet"]["port"],
+            "workers": config["resources"]["wordnet"]["workers"],
+        },
+        **default_args
+    )
 
 
 if __name__ == "__main__":
